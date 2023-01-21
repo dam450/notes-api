d7e1904b6890ceb0ee0c3abdc10dccbc393f4794
Dam450 <68165752+dam450@users.noreply.github.com>
2023-01-20 21:53:17 -0300

    fix: repeated notes on index route

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -74,7 +74,7 @@ class NotesController {
         .whereLike('notes.title', `%${title}%`)
         .whereIn('name', filterTags)
         .innerJoin('notes', 'notes.id', 'tags.note_id')
-        .orderBy('notes.title')
+        .orderBy('notes.title').groupBy('notes.id')
 
 
     } else {
