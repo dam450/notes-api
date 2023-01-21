313d3b700ce98c645ce8f6374b9db25ec8a2ad94
Dam450 <68165752+dam450@users.noreply.github.com>
2023-01-20 21:01:43 -0300

    knex query log

--- src/database/knex/index.js
+++ src/database/knex/index.js
@@ -3,4 +3,8 @@ const knex = require('knex')
 
 const connection = knex(config.development)
 
+// connection.on('query', queryData => {
+//   console.log('[ Knex ] ', queryData.sql, queryData.bindings)
+// })
+
 module.exports = connection
